174b832f0afd519c179573dd380c53036028f796
Vladislav Dmitriyev <d7vladislav@gmail.com>
2019-05-28 18:17:35 +0300

    Prepared OpenAPI specification

---
+++ cmd/avitornd/avitornd.go
@@ -0,0 +1,11 @@
+/*
+Copyright 2019 Vladislav Dmitriyev.
+*/
+
+// Package avitornd provides command-line interface to start REST API
+// server for generating random values by specified type and length params.
+package avitornd
+
+func main(){
+
+}
